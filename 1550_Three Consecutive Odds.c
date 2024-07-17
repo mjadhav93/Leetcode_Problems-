7ef8bfa05b342babf768d14c7703f6702bb5e237
mjadhav93 <163880871+mjadhav93@users.noreply.github.com>
2024-07-17 11:55:06 -0400

    Add files via upload

---
+++ 1550_Three Consecutive Odds.c
@@ -0,0 +1,35 @@
+/*Create 1550. Three Consecutive Odds
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+ 
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+ 
+Constraints:
+
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000*/
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool threeConsecutiveOdds(int arr[], int arrSize) {
+//Iterate through the array up to the third last element
+    for(int i =0; i < arrSize - 2; i++)
+    {
+    //Check if the current element and the next two elements are odd
+        if(arr[i] % 2 !=0 && arr[i+1] % 2 !=0 && arr[i+2] % 2 !=0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
